075957624918ab1c158edc7e12968fa3ce781b35
smirnov <shramru@gmail.com>
2017-11-24 00:42:45 +0300

    .

--- src/track_handler.cpp
+++ src/track_handler.cpp
@@ -47,7 +47,7 @@ void TrackHandler::handleRequest(Poco::Net::HTTPServerRequest& request, Poco::Ne
     std::time_t time = std::stol(it->second, nullptr, 10);
 
     const auto query = format("SELECT toString(time), latitude, longitude, accuracy, speed FROM tracking.logs "
-                                      "WHERE id = '{}' AND time BETWEEN {} AND {} ORDER BY time",
+                                      "WHERE id = '{}' AND time BETWEEN '{}' AND '{}' ORDER BY time",
                               user.id, today(time), tomorrow(time));
     auto array = Json::array();
     client.Select(query,
